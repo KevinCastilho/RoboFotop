143c60cff59e8bf52c3bbd5d7d736e3635b5b761
thiagopivatto <thiago.pivatto@outlook.com>
2023-05-30 04:42:13 -0300

    Commit

--- src/test/java/com/projeto_automacao/tests/Selenium_Tests.java
+++ src/test/java/com/projeto_automacao/tests/Selenium_Tests.java
@@ -17,5 +17,37 @@ public class Selenium_Tests extends Selenium_Methods{
         sendKeysWithHighlight(campoPesquisa, "Teste de Software");
         campoPesquisa.sendKeys(Keys.ENTER);
     }
+
+    @Test
+    public void Pesquisa_Google2() throws Exception {
+        WebElement campoPesquisa = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"));
+        waitForElementPresent(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"), 5);
+        sendKeysWithHighlight(campoPesquisa, "Teste de Software");
+        campoPesquisa.sendKeys(Keys.ENTER);
+    }
+
+    @Test
+    public void Pesquisa_Google3() throws Exception {
+        WebElement campoPesquisa = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"));
+        waitForElementPresent(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"), 5);
+        sendKeysWithHighlight(campoPesquisa, "Teste de Software");
+        campoPesquisa.sendKeys(Keys.ENTER);
+    }
+
+    @Test
+    public void Pesquisa_Google4() throws Exception {
+        WebElement campoPesquisa = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"));
+        waitForElementPresent(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"), 5);
+        sendKeysWithHighlight(campoPesquisa, "Teste de Software");
+        campoPesquisa.sendKeys(Keys.ENTER);
+    }
+
+    @Test
+    public void Pesquisa_Google5() throws Exception {
+        WebElement campoPesquisa = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"));
+        waitForElementPresent(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/textarea"), 5);
+        sendKeysWithHighlight(campoPesquisa, "Teste de Software");
+        campoPesquisa.sendKeys(Keys.ENTER);
+    }
 }
 
